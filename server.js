fcf231eae7554fd017f1aaf85140a7ae5c28d556
channaveerayya <channucs0009@gmail.com>
2021-03-03 18:04:40 +0530

    server changed

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
-const favicon = require('express-favicon')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
-app.use(favicon(__dirname + '/build/favicon.ico'))
-app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'build')))
-app.get('/ping', function (req, res) {
-  return res.send('pong')
-})
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+}
 app.listen(port)
